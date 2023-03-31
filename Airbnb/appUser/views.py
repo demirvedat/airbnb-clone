e03c959ff7e5e4724c0577d28247a7337502fb0b
Vedat Demir <dilsardemir@gmail.com>
2023-03-31 23:44:33 +0300

    changes on melodi's page

--- Airbnb/appUser/views.py
+++ Airbnb/appUser/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def loginUser(request):
+    
+    return redirect('Index')
+
+def registerUser(request):
+    
+    return redirect('Index')
